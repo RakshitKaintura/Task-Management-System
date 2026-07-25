cf46abe10de366e37b152cc196f80c8461e64e41
RakshitKaintura <blank1951k@gmail.com>
2026-07-26 00:08:12 +0530

    logout fix

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -24,6 +24,7 @@ const useAuthStore = create<AuthState>((set) => ({
       console.error('Logout failed', e);
     }
     set({ user: null, isAuthenticated: false });
+    window.location.href = '/login';
   },
   
   fetchUser: async () => {
